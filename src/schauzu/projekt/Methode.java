c8f1db48f0b53f7fc9b7137366750de04727363d
Omer Tufan <omertufanca@gmail.com>
2022-04-22 12:03:48 +0200

    22.04.22

--- src/schauzu/projekt/Methode.java
+++ src/schauzu/projekt/Methode.java
@@ -4,15 +4,23 @@ import java.sql.*;
 import java.util.Scanner;
 
 public class Methode {
-	
-	private static String benutzerName="root";
-	private static String pass="Pass.4150";
+
+	private final static String BENUTZURNAME = "root";
+	private final static String PASS = "Pass.4150";
+
+	public static String getBenutzurname() {
+		return BENUTZURNAME;
+	}
+
+	public static String getPass() {
+		return PASS;
+	}
 
 	public void menu(String filmtitel) throws ClassNotFoundException, SQLException {
 
 		Scanner scan = new Scanner(System.in);
 		String film = filmtitel;
-		int auswahl = 0;
+		String auswahl = "";
 
 		while (!hasFilmtitel(film)) {
 			System.out.println(
@@ -26,35 +34,35 @@ public class Methode {
 					"\n1-Dauer \n2-Hauptdarsteller \n3-Filmstudio \n4-Land \n5-Genre \n6-Inhaltbeschreibung \n7-neuen Film eingeben \n8-Exit");
 
 			System.out.println("Welche Information möchten Sie erreichen ? ");
-			auswahl = scan.nextInt();
+			auswahl = scan.next();
 
 			switch (auswahl) {
-			case 1:
+			case "1":
 				findDauer(film);
 				break;
-			case 2:
+			case "2":
 				findHauptdarsteller(film);
 				break;
-			case 3:
+			case "3":
 				findFilmstudio(film);
 				break;
-			case 4:
+			case "4":
 				findLand(film);
 				break;
-			case 5:
+			case "5":
 				findGenre(film);
 				break;
-			case 6:
+			case "6":
 				findInhaltbeschreibung(film);
 				break;
-			case 7:
+			case "7":
 				getAllFilmtitel();
 				do {
 					film = neuerFilmtitel();
 				} while (!hasFilmtitel(film));
-						
+
 				break;
-			case 8:
+			case "8":
 				System.out.println("Vielen Dank ! ");
 				break;
 			default:
@@ -62,7 +70,7 @@ public class Methode {
 				break;
 			}
 
-		} while (auswahl != 8);
+		} while (!auswahl.equals("8"));
 
 	}
 
@@ -71,7 +79,7 @@ public class Methode {
 		Class.forName("com.mysql.cj.jdbc.Driver");
 
 		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/schauzu_projekt?serverTimezone=UTC",
-				benutzerName, pass);
+				BENUTZURNAME, PASS);
 
 		Statement st = con.createStatement();
 
@@ -93,7 +101,7 @@ public class Methode {
 		Class.forName("com.mysql.cj.jdbc.Driver");
 
 		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/schauzu_projekt?serverTimezone=UTC",
-				benutzerName, pass);
+				BENUTZURNAME, PASS);
 
 		Statement st = con.createStatement();
 
@@ -117,7 +125,7 @@ public class Methode {
 		Class.forName("com.mysql.cj.jdbc.Driver");
 
 		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/schauzu_projekt?serverTimezone=UTC",
-				benutzerName, pass);
+				BENUTZURNAME, PASS);
 
 		Statement st = con.createStatement();
 
@@ -141,7 +149,7 @@ public class Methode {
 		Class.forName("com.mysql.cj.jdbc.Driver");
 
 		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/schauzu_projekt?serverTimezone=UTC",
-				benutzerName, pass);
+				BENUTZURNAME, PASS);
 
 		Statement st = con.createStatement();
 
@@ -167,7 +175,7 @@ public class Methode {
 		Class.forName("com.mysql.cj.jdbc.Driver");
 
 		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/schauzu_projekt?serverTimezone=UTC",
-				benutzerName, pass);
+				BENUTZURNAME, PASS);
 
 		Statement st = con.createStatement();
 
@@ -192,7 +200,7 @@ public class Methode {
 		Class.forName("com.mysql.cj.jdbc.Driver");
 
 		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/schauzu_projekt?serverTimezone=UTC",
-				benutzerName, pass);
+				BENUTZURNAME, PASS);
 
 		Statement st = con.createStatement();
 
@@ -218,7 +226,7 @@ public class Methode {
 		Class.forName("com.mysql.cj.jdbc.Driver");
 
 		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/schauzu_projekt?serverTimezone=UTC",
-				benutzerName, pass);
+				BENUTZURNAME, PASS);
 
 		Statement st = con.createStatement();
 
@@ -245,7 +253,7 @@ public class Methode {
 		Class.forName("com.mysql.cj.jdbc.Driver");
 
 		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/schauzu_projekt?serverTimezone=UTC",
-				benutzerName, pass);
+				BENUTZURNAME, PASS);
 
 		Statement st = con.createStatement();
 
@@ -268,7 +276,7 @@ public class Methode {
 		Class.forName("com.mysql.cj.jdbc.Driver");
 
 		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/schauzu_projekt?serverTimezone=UTC",
-				benutzerName, pass);
+				BENUTZURNAME, PASS);
 
 		Statement st = con.createStatement();
 		PreparedStatement ps = con.prepareStatement("INSERT INTO film VALUES(?,?,?,?)");
@@ -277,9 +285,9 @@ public class Methode {
 		String eingabe = "";
 
 		Film film = new Film();
-		
+
 		System.out.println("Bitte geben Sie den Filmtitel ein : ");
-		
+
 		film.setFilmtitel(scan.nextLine());
 
 		while (hasFilmtitel(film.getFilmtitel())) {
@@ -332,12 +340,42 @@ public class Methode {
 		do {
 			System.out.println("Bitte geben Sie die ID Nummer des Filmstudios ein :");
 
-			
 			id = scan.nextInt();
 		} while (id < 10 || id > 14);
-		
-		System.out.println("ausgewaehlte ID-Nummer :" +id);
+
+		System.out.println("ausgewaehlte ID-Nummer :" + id);
 		return id;
+	}
+
+	public void findFilmtitelImArray(Film[] filmList) {
+
+		Scanner scan = new Scanner(System.in);
+		int flag = 0;
+		String auswahl="";
+
+		String filmTitel = "";
+
+		do {
+		System.out.println("\nBitte geben Sie einen Filmtitel ein : ");
+		filmTitel = scan.nextLine();
+
+		for (Film film : filmList) {
+
+			if (film.getFilmtitel().equalsIgnoreCase(filmTitel)) {
+				flag++;
+				System.out.println(film);
+			}
+
+		}
+		
+		if (flag==0) {
+			System.out.println("Der eingegebene Film befindet sich nicht im Array !");
+		}
+		
+		System.out.println("Möchten Sie einen neuen Filmtitel eingeben ? (ja/nein) ");
+		auswahl=scan.nextLine();
+		
+		} while (auswahl.equalsIgnoreCase("ja"));	
 	}
 
 }
